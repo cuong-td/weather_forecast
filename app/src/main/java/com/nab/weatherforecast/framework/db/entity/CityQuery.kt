125d461c1f157c6ad9a011a61550bdc097bf9f76
Cuong Thai <thaiduycuong@gmail.com>
2021-09-04 17:45:43 +0700

    Update table primary keys

--- app/src/main/java/com/nab/weatherforecast/framework/db/entity/CityQuery.kt
+++ app/src/main/java/com/nab/weatherforecast/framework/db/entity/CityQuery.kt
@@ -2,10 +2,10 @@ package com.nab.weatherforecast.framework.db.entity
 
 import androidx.room.Entity
 
-@Entity(tableName = "city_query", primaryKeys = ["query, timestamp"])
+@Entity(tableName = "city_query", primaryKeys = ["query", "timestamp"])
 data class CityQuery(
     val cityId: Long = 0L,
     val query: String = "",
     val name: String = "",
-    val timestamp: Long = 0
+    val timestamp: Long = 0L
 )
